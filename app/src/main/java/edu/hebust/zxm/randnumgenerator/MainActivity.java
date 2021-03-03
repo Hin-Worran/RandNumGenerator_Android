efe8eefcc3c3e39736e3406ba12eb32d6f16c0ac
Hin <873243610@qq.com>
2021-03-03 23:29:15 +0800

    enable to set minimum and maximum

--- app/src/main/java/edu/hebust/zxm/randnumgenerator/MainActivity.java
+++ app/src/main/java/edu/hebust/zxm/randnumgenerator/MainActivity.java
@@ -3,12 +3,36 @@ package edu.hebust.zxm.randnumgenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    Random r=new Random();
+    int minNum;
+    int maxNum;
+    int randNum;
+    TextView tvOut;
+    Button btGenerate;
+    EditText etMin;
+    EditText etMax;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvOut=(TextView)findViewById(R.id.tv_RanNum);
+        btGenerate=(Button)findViewById(R.id.btn_generate);
+        etMin=(EditText)findViewById(R.id.et_min);
+        etMax=(EditText)findViewById(R.id.et_max);
+    }
+
+    public void Generate(View view) {
+        minNum=Integer.parseInt(etMin.getText().toString());
+        maxNum=Integer.parseInt(etMax.getText().toString());
+        randNum=minNum+r.nextInt(maxNum-minNum+1);
+        tvOut.setText("随机数是"+randNum);
     }
 }
